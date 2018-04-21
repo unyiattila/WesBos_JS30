b8004260d0fe6e21dca19b2f40afdf021b8094e6
unyiattila <unyiattila@gmail.com>
2018-04-21 20:34:04 +0200

    First day completed.

--- JS30/01 - JavaScript Drum Kit/app.js
+++ JS30/01 - JavaScript Drum Kit/app.js
@@ -2,5 +2,11 @@ function playSound(e) {
   let keySound = document.querySelector(`[id="${e.keyCode}"]`)
   keySound.currentTime = 0;
   keySound.play();
+
+  keySound.parentElement.classList.add("transitionStart")
+  keySound.parentElement.addEventListener("transitionend", function(){
+    keySound.parentElement.classList.remove("transitionStart");
+  });
+
 }
 window.addEventListener('keydown', playSound)
